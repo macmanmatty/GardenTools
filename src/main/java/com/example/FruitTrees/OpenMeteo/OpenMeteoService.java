6bfc9aca84243306b8fec79d64e815e03209733a
“Jesse <jamatty@gmail.com>
2024-02-18 13:31:20 -0600

    added cacheable annotation and seperate Method

--- src/main/java/com/example/FruitTrees/OpenMeteo/OpenMeteoService.java
+++ src/main/java/com/example/FruitTrees/OpenMeteo/OpenMeteoService.java
@@ -1,5 +1,4 @@
 package com.example.FruitTrees.OpenMeteo;
-
 import com.example.FruitTrees.ChillingHours.WeatherDataProcessor;
 import com.example.FruitTrees.Location.Location;
 import com.example.FruitTrees.WeatherConroller.BadRequestException;
@@ -8,16 +7,18 @@ import com.example.FruitTrees.WeatherConroller.WeatherRequest;
 import com.example.FruitTrees.WeatherConroller.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
-
+/**
+ * service class for making Open-Meteo requests
+ */
 @Service
 public class OpenMeteoService {
     @Value("${open-meteo.url}")
@@ -26,22 +27,17 @@ public class OpenMeteoService {
     private  boolean processMultipleLocationRequestsEnabled;
     @Value("${max.Multiple.OpenMeteo.Requests}")
     private  int maxMultipleOpenMeteoRequests;
-   private  RestTemplate restTemplate = new RestTemplate();
-
-   private WeatherDataProcessor weatherDataProcessor;
-
-
+   private final  RestTemplate restTemplate = new RestTemplate();
+   private  final WeatherDataProcessor weatherDataProcessor;
     public OpenMeteoService( @Autowired  WeatherDataProcessor weatherDataProcessor) {
         this.weatherDataProcessor = weatherDataProcessor;
     }
-
     public WeatherResponse getData(WeatherRequest weatherRequest ) throws IOException {
         extractAdditionalDataTypes(weatherRequest);
         OpenMeteoLocationResponses openMeteoResponses=makeRequest(weatherRequest);
       WeatherResponse  response= weatherDataProcessor.processHourlyData( weatherRequest, openMeteoResponses);
         return  response;
     }
-
     private void extractAdditionalDataTypes(WeatherRequest weatherRequest) {
         List<HourlyWeatherProcessRequest> hourlyWeatherProcessRequests=weatherRequest.hourlyWeatherProcessRequests;
         for(HourlyWeatherProcessRequest hourlyWeatherProcessRequest:hourlyWeatherProcessRequests){
@@ -49,36 +45,55 @@ public class OpenMeteoService {
         }
     }
 
+    /**
+     * call the open-meteo service to get the data for specified location(s)
+     * in the weather request one open-meteo request is required per location
+     *
+     * @param weatherRequest the weather request objcet
+     * @return
+     * @throws IOException
+     */
     private OpenMeteoLocationResponses makeRequest(WeatherRequest weatherRequest) throws IOException {
       OpenMeteoLocationResponses openMeteoResponses=  new OpenMeteoLocationResponses();
       List<Location> locations=weatherRequest.getLocations();
       locationCheck(locations);
       for (Location location: locations) {
           try {
-              String fullUrl = openMeteoUrl + "?latitude=" + location.getLatitude() +
-                      "&longitude=" + location.getLongitude() +
-                      "&start_date=" + weatherRequest.getStartDate() +
-                      "&end_date=" + weatherRequest.getEndDate();
-              Set<String> hourlyDataTypes = weatherRequest.getHourlyDataTypes();
-              for (String dataType : hourlyDataTypes) {
-                  fullUrl = fullUrl + "&hourly=" + dataType;
-              }
-              fullUrl = addConversionUnits(fullUrl, weatherRequest);
-              Logger.getLogger("").info("full url " + fullUrl);
-
-              ResponseEntity<OpenMeteoResponse> response = restTemplate.getForEntity(fullUrl, OpenMeteoResponse.class);
-              LocationResponse locationResponse=new LocationResponse();
-              locationResponse.setOpenMeteoResponse(response.getBody());
-              locationResponse.setLocation(location);
+             LocationResponse locationResponse=makeLocationRequest(location, weatherRequest);
                openMeteoResponses.getLocationResponses().add(locationResponse);
           } catch (RestClientException e) {
               throw new IOException(e);
           }
       }
-
       return  openMeteoResponses;
     }
 
+
+    @Cacheable("weather")
+    public LocationResponse makeLocationRequest( Location location, WeatherRequest weatherRequest){
+        String fullUrl = openMeteoUrl + "?latitude=" + location.getLatitude() +
+                "&longitude=" + location.getLongitude() +
+                "&start_date=" + weatherRequest.getStartDate() +
+                "&end_date=" + weatherRequest.getEndDate();
+        Set<String> hourlyDataTypes = weatherRequest.getHourlyDataTypes();
+        for (String dataType : hourlyDataTypes) {
+            fullUrl = fullUrl + "&hourly=" + dataType;
+        }
+        fullUrl = addConversionUnits(fullUrl, weatherRequest);
+        Logger.getLogger("").info("full url " + fullUrl);
+        ResponseEntity<OpenMeteoResponse> response = restTemplate.getForEntity(fullUrl, OpenMeteoResponse.class);
+        LocationResponse locationResponse=new LocationResponse();
+        locationResponse.setOpenMeteoResponse(response.getBody());
+        locationResponse.setLocation(location);
+            return locationResponse;
+    }
+
+
+    /**
+     * checks to make sure the number of locations
+     * you are attempting to retrieve  data for is allowed
+     * @param locations the list of locations
+     */
     private void locationCheck(List<Location> locations) {
         int size=locations.size();
         if (size==0){
@@ -86,13 +101,19 @@ public class OpenMeteoService {
         }
         if(size>1 && !processMultipleLocationRequestsEnabled){
             throw new BadRequestException("You may only process one location at time currently");
-
         }
         else if(size>maxMultipleOpenMeteoRequests){
             throw new BadRequestException("You may only process "+maxMultipleOpenMeteoRequests+ " locations at time currently");
         }
     }
 
+    /**
+     * additional params to the url to specify the units
+     * you wish to have data sent back in Fahrenheit, Celsius, Inches , Meters Etc.
+     * @param openMeteoUrl the request url for open meteo
+     * @param weatherRequest the weather request object
+     * @return
+     */
     public String addConversionUnits(String openMeteoUrl, WeatherRequest weatherRequest){
         String temperatureUnit=weatherRequest.getTemperatureUnit();
         String windSpeedUnit=weatherRequest.getWindSpeedUnit();
@@ -108,5 +129,4 @@ public class OpenMeteoService {
             }
          return    openMeteoUrl;
   }
-
 }
