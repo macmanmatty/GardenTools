a0f3f8d75604cfa39efe77fba5d1b66cf9e1ba64
jessematty <jamatty@gmail.com>
2025-08-11 19:41:54 -0500

    removed star import

--- src/main/java/com/example/FruitTrees/WeatherProcessor/WeatherProcessorService.java
+++ src/main/java/com/example/FruitTrees/WeatherProcessor/WeatherProcessorService.java
@@ -11,9 +11,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+
 
 @Service
 public class WeatherProcessorService {
